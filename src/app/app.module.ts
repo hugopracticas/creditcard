5f9b7697124e1b8b866dfa5c07b3b14e3142a715
hugopracticas <hugoloyolamaya@gmail.com>
2021-10-19 17:16:13 -0500

    Se inicia desarrollo de formularios reactivos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,20 @@
 import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import { ReactiveFormsModule } from '@angular/forms';
+import { BrowserModule } from '@angular/platform-browser'; //Para uso de formularios reactivos
 
 import { AppComponent } from './app.component';
+import { CardFormComponent } from './card-form/card-form.component';
+import { InputComponent } from './input/input.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CardFormComponent,
+    InputComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    ReactiveFormsModule //Se importa para poder hacer uso de formularios reactivos
   ],
   providers: [],
   bootstrap: [AppComponent]
